5009d17c5827533fa2329c3ea4db3ce73900fc63
suryaaris <kusumaaris2004@gmail.com>
2024-06-27 00:56:34 +0800

    Add files via upload

---
+++ dump.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 26, 2024 at 04:52 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dump`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `ordersummary`
+-- (See below for the actual view)
+--
+CREATE TABLE `ordersummary` (
+`Order_id` int(11)
+,`Customer_nama` varchar(100)
+,`tanggal_order` date
+,`nama_produk` varchar(100)
+,`kuantitas` int(11)
+,`harga` decimal(10,2)
+,`total_harga` decimal(20,2)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `produk_id` int(11) NOT NULL,
+  `nama_produk` varchar(100) DEFAULT NULL,
+  `harga` decimal(10,2) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`produk_id`, `nama_produk`, `harga`, `stok`) VALUES
+(1, 'Wortel', 10000.00, 30),
+(2, 'Tomat', 5000.00, 20),
+(3, 'Bayam', 6000.00, 40);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_customer`
+--
+
+CREATE TABLE `tb_customer` (
+  `customer_id` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  `no_telp` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_customer`
+--
+
+INSERT INTO `tb_customer` (`customer_id`, `nama`, `alamat`, `no_telp`) VALUES
+(1, 'Aris', 'Jl.Antasura', '08193311813'),
+(2, 'Andhi', 'Jl.Arjuna', '0831937113'),
+(3, 'Riow', 'Jl.Pahlawan', '0130131301');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_detail`
+--
+
+CREATE TABLE `tb_detail` (
+  `detail_id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `produk_id` int(11) DEFAULT NULL,
+  `kuantitas` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_detail`
+--
+
+INSERT INTO `tb_detail` (`detail_id`, `order_id`, `produk_id`, `kuantitas`) VALUES
+(1, 1, 1, 10),
+(2, 1, 2, 5),
+(3, 2, 3, 15);
+
+--
+-- Triggers `tb_detail`
+--
+DELIMITER $$
+CREATE TRIGGER `stok_kurang` AFTER INSERT ON `tb_detail` FOR EACH ROW BEGIN
+    UPDATE produk
+    SET stok = stok - NEW.kuantitas
+    WHERE produk_id = NEW.produk_id;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_order`
+--
+
+CREATE TABLE `tb_order` (
+  `order_id` int(11) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `tanggal_order` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_order`
+--
+
+INSERT INTO `tb_order` (`order_id`, `customer_id`, `tanggal_order`) VALUES
+(1, 1, '2024-06-28'),
+(2, 2, '2024-06-30');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `ordersummary`
+--
+DROP TABLE IF EXISTS `ordersummary`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `ordersummary`  AS SELECT `o`.`order_id` AS `Order_id`, `c`.`nama` AS `Customer_nama`, `o`.`tanggal_order` AS `tanggal_order`, `p`.`nama_produk` AS `nama_produk`, `od`.`kuantitas` AS `kuantitas`, `p`.`harga` AS `harga`, `od`.`kuantitas`* `p`.`harga` AS `total_harga` FROM (((`tb_order` `o` join `tb_customer` `c` on(`o`.`customer_id` = `c`.`customer_id`)) join `tb_detail` `od` on(`o`.`order_id` = `od`.`order_id`)) join `produk` `p` on(`od`.`produk_id` = `p`.`produk_id`)) ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`produk_id`),
+  ADD KEY `idx_nama_produk` (`nama_produk`);
+
+--
+-- Indexes for table `tb_customer`
+--
+ALTER TABLE `tb_customer`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `tb_detail`
+--
+ALTER TABLE `tb_detail`
+  ADD PRIMARY KEY (`detail_id`),
+  ADD KEY `idx_order_id` (`order_id`),
+  ADD KEY `idx_produk_id` (`produk_id`);
+
+--
+-- Indexes for table `tb_order`
+--
+ALTER TABLE `tb_order`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `idx_customer_id` (`customer_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `produk_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_customer`
+--
+ALTER TABLE `tb_customer`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_detail`
+--
+ALTER TABLE `tb_detail`
+  MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_order`
+--
+ALTER TABLE `tb_order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_detail`
+--
+ALTER TABLE `tb_detail`
+  ADD CONSTRAINT `tb_detail_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `tb_order` (`order_id`),
+  ADD CONSTRAINT `tb_detail_ibfk_2` FOREIGN KEY (`produk_id`) REFERENCES `produk` (`produk_id`);
+
+--
+-- Constraints for table `tb_order`
+--
+ALTER TABLE `tb_order`
+  ADD CONSTRAINT `tb_order_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `tb_customer` (`customer_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
